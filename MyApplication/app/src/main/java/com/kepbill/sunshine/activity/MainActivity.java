206a0b3186e27e7c0342dcc4ade98223b588a1fb
luisgonzalezmac <luis.gonzalez.espejo@gmail.com>
2016-07-09 03:38:22 -0300

    inicio

--- MyApplication/app/src/main/java/com/kepbill/sunshine/activity/MainActivity.java
+++ MyApplication/app/src/main/java/com/kepbill/sunshine/activity/MainActivity.java
@@ -1,12 +1,11 @@
 package com.kepbill.sunshine.activity;
 
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.kepbill.sunshine.R;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
